4d81fb307ca038a9d42199d5a9f14984b87c7d7a
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-02-19 23:22:49 +0100

    - Création de la vue du formulaire de connexion

---
+++ apprdp/views/userslog/loginView.php
@@ -0,0 +1,31 @@
+<div class="container">
+	<main>
+		<h2>Connectez-vous à votre espace personnel pour profiter de toutes les avantages du site</h2>
+		<form class="registerForm" action=<?= base_url('userslog/login') ?> method="post">
+			<div class="form-group row">
+				<label for="email" class="col-lg-2 col-sm-2 col-form-label">Email</label>
+				<div class="col-lg-5 col-sm-8">
+					<input type="email" class="form-control" id="email" name="email" value="<?php if(form_error('email') == NULL){echo set_value('email');} ?>" placeholder="Email">
+				</div>
+				<span><?= form_error('email'); ?></span>
+			</div>
+			<div class="form-group row">
+				<label for="password" class="col-lg-2 col-sm-2 col-form-label">Mot de passe</label>
+				<div class="col-lg-5 col-sm-8">
+					<input type="password" class="form-control" id="password" name="password" placeholder="Mot de passe">
+				</div>
+				<span><?= form_error('password'); ?></span>
+			</div>
+
+			<div class="form-group row">
+			    <div class="col-lg-5 col-sm-8">
+			    	<input type="submit" class="btn btn-primary" value ="Se connecter">
+			    </div>
+		  	</div>
+			<div class="">
+				<a href="#">Mot de passe oublié?</a>
+				<a href="<?= base_url('inscription/formulaire') ?>">Je crée mon compte en quelques clics</a>
+			</div>
+		</form>
+	</main>
+</div>
