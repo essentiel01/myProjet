aa2487f871a2aef37c2a3720580cccd72108bc33
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-02-20 20:15:39 +0100

    - Création de la vue del'espace personnel.

---
+++ apprdp/views/userslog/espacePersonnelView.php
@@ -0,0 +1,6 @@
+<div class="container">
+	<h2>Bienvenue dans votre espace personnel</h2>
+	<a href="#">Mes infromations</a>
+	<a href="#">Mes revues favoris</a>
+	<a href="#">Mes chroniques favoris</a>
+</div>
