f4e1bd4b6894adb1f596aaff19db48d59b0d300f
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-02-20 20:02:56 +0100

    - Retrait de la library session du constructeur. - Modification et ajout de commentaires. - Modification de la function login(). Stockage de données utilisateur dans $_SESSION. Affichage des vues et des templates en fonction de la connexion ou non d'un utilisateur. - Création de la function logout(). - Création de la function userDashboard().

--- apprdp/controllers/UsersLog.php
+++ apprdp/controllers/UsersLog.php
@@ -10,19 +10,30 @@ class UsersLog extends CI_Controller
 		parent::__construct();
 		$this->load->model('users_model');
 		$this->load->helper('url');
-		$this->load->library(array('form_validation', 'session'));
+		$this->load->library(array('form_validation'));
 	}
 
+	/**
+	 * affiche le formulaire de connexion des utilsateurs
+	 */
 	public function index()
 	{
+		//variables à transmettre à la vue
 		$data['login'] = array(
 			'headerTitle' => 'Connexion espace personnel'
 		);
+
+		//header
 		$this->load->view('templates/header', $data['login']);
+		//formulaire de connexion
 		$this->load->view('userslog/loginView');
+		//footer
 		$this->load->view('templates/footer');
 	}
 
+	/**
+	 * Valide les données du formulaire de connexion et vérifie si l'utilisateur a un compte dans la base de données. Si les données du formulaire ne respectent pas les règles de validation le formulaire est réaffiché avec les messages d'erreur de validation. Si les données respectent les règles de validation et que l'utilisateur existe dans la base de données alors une page de succès est affichée. Si l'utilisateur n'existe pas dans la base de données ou que le mot de passe qu'il a saisi est incorrecte alors le formulaire est réaffiché avec un message d'erreur
+	 */
 	public function logIn()
 	{
 
@@ -32,31 +43,96 @@ class UsersLog extends CI_Controller
 
 			//variables à transmettre à la vue
 			$data['registerError'] = array(
-				'headerTitle' => 'Données incorrectes / formulaire de connexion'
+				'headerTitle' => 'Données incorrectes / formulaire de connexion' // titre du header
 			);
 
-			//chargement des vues
+			//header
 			$this->load->view('templates/header', $data['registerError']);
+			//formulaire de connexion
 			$this->load->view('userslog/loginView');
+			//footer
+			$this->load->view('templates/footer');
 
 		} else {
-			// tableau des données à enregistrer
+			// les identifiants de l'utilisateur à vérfier dans la base
 			$queryParams = array(
 				'from' => 'users',
 				'where1' => array('userEmail' => $_POST['email']),
 				'password' => $_POST['password']
 			);
 
-			$result = $this->users_model->userAuthentificate($queryParams);
-			//var_dump($result);
-				//var_dump($_SESSION);
-			if (is_array($result)) {
-				$_SESSION['userData'] = $result;
-			}else {
-				echo $result;
+			// si la requête renvoie un objet on le stocke dans $_SESSION
+			if (is_object($this->users_model->userAuthentificate($queryParams))) {
+
+				$_SESSION['userData'] = $this->users_model->userAuthentificate($queryParams);
+
+			} else { // sinon on affiche le message d'erreur
+
+				$loginError = $this->users_model->userAuthentificate($queryParams);
+
 			}
 
+			// si un utilisateur est connecté on load le headerLogged
+			if (isset($_SESSION['userData'])){
+
+				// variable à transmettre à la vue
+				$data['user'] = array(
+					'headerTitle' => 'Connexion rèussie'
+				);
+
+				//headerLogged
+				$this->load->view('templates/headerLogged', $data['user']);
+				//page de succès de connexion
+				$this->load->view('userslog/success');
+				//footer
+				$this->load->view('templates/footer');
+
+			} else { // si personne n'est connectée on load le header et on réaffiche le formulaire de connexion.
+
+				// variable à transmettre à la vue
+				$data['user'] = array(
+					'headerTitle' => 'Données incorrectes / formulaire de connexion',
+					'loginError' => $loginError
+				);
+
+				//header
+				$this->load->view('templates/header', $data['user']);
+				//formulaire de connexion
+				$this->load->view('userslog/loginView');
+				//footer
+				$this->load->view('templates/footer');
+			}
 		}
+	}
+
+
+	/**
+	 * Déconnexion de l'utilisateur
+	*/
+	public function logout()
+	{
+		$this->session->sess_destroy();
+
+		redirect('culture'); // TODO la redirection doit être faite vers l'accueil
+	}
+
+
+	/**
+	 * affiche l'espace personnel de l'utilisateur connecté
+	*/
+	public function userDashboard()
+	{
+		// variable à transmettre à la vue
+		$data['user'] = array(
+			'headerTitle' => 'Espace personnel',
+		);
+
+		//headerLogged
+		$this->load->view('templates/headerLogged', $data['user']);
+		//vue de l'espace personnel
+		$this->load->view('userslog/espacePersonnelView');
+		//footer
+		$this->load->view('templates/footer');
 	}
 }
 
