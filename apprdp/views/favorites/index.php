fa27c090954b19f1d90c7c970b4cae15e380f69d
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-02-25 15:48:41 +0100

    - création de la vue index.php des favoris

---
+++ apprdp/views/favorites/index.php
@@ -0,0 +1,6 @@
+<div class="container">
+	<main>
+		<a href="<?= base_url('espace-personnel/mes-revues-de-presse-favoris') ?>" class="btn btn-lg" role="button">Ma sélection de revues de presse</a>
+		<a href="<?= base_url('espace-personnel/mes-chroniques-favoris') ?>" class="btn btn-lg" role="button">Ma sélection de chronique</a>
+	</main>
+</div>
