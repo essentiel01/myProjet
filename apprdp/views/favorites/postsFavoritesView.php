6ae8ea15a42feca42e91920ddc3018d2c8836f8d
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-02-25 19:39:00 +0100

    - ajout de la condition if!empty() pour pouvoir afficher un message quand laliste des favoris sera vide.

--- apprdp/views/favorites/postsFavoritesView.php
+++ apprdp/views/favorites/postsFavoritesView.php
@@ -1,19 +1,27 @@
 <div class="container">
 	<main>
 		<h2><?= $mainTitle ?></h2>
-		<?php foreach ($favoritesList as $row) : ?>
+		<!-- si la liste des favoris n'est pas vide on l'affiche -->
+		<?php if(!empty($favoritesList)) {
+			 foreach ($favoritesList as $row) : ?>
 
-			<form class="">
-				<input type="checkbox" name="favoriteCheckbox" value="">
-				<p><?= $row->countryName ?></p>
-				<p>Rubrique: <?= $row->categoryName ?></p>
-				<p>Titre: <?= $row->postTitle ?></p>
-			</form>
-			<div class="">
-				<a href="<?= base_url('culture/publication/' . $row->postId . '/' . $row->postSlug); ?>" class="btn btn-success btn-lg">Lire</a>
-				<a href="#" class="btn btn-success btn-lg">Ecouter</a>
-				<a href="#" class="btn btn-danger btn-lg">Supprimer</a>
-			</div>
-		<?php endforeach ?>
+				<form class="" method="post" action="<?= base_url('favorites/deletepost') ?>">
+					<input type="checkbox" name="favoriteCheckbox" value="">
+					<input type="hidden" name="postId" value="<?= $row->postId ?>">
+					<p><?= $row->countryName ?></p>
+					<p>Rubrique: <?= $row->categoryName ?></p>
+					<p>Titre: <?= $row->postTitle ?></p>
+					<div class="">
+						<a href="<?= base_url('culture/publication/' . $row->postId . '/' . $row->postSlug); ?>" class="btn btn-success btn-lg">Lire</a>
+						<a href="#" class="btn btn-success btn-lg">Ecouter</a>
+						<input type="submit"  class="btn btn-danger btn-lg" value="Supprimer" />
+					</div>
+				</form>
+			<?php endforeach ;
+		} else {
+			// sinon on affiche un message de liste vide
+			if(isset ($emptyFavoritesList)) { echo '<p>' . $emptyFavoritesList . '</p>';}
+		} ?>
+
 	</main>
 </div>
