fe769b6fb22c6a8ee8783078bf8e2953ae653634
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-02-25 15:42:01 +0100

    - ajout du composant modal de bootstrap pour ajouter un favoris. - ajout d'une condition if pour marquer les revues favoris d'un petit coeur.

--- apprdp/views/culture/singleView.php
+++ apprdp/views/culture/singleView.php
@@ -1,10 +1,44 @@
 <div class="container">
 	<main class="mainWrapper">
 		<?php foreach ($post as $row) : ?>
-				<a href="#" class="btn btn-success btn-lg " role="button" aria-pressed="true">Ajouter à mes favoris</a>
+					<!-- Button trigger modal -->
+					<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+					  Favoris
+					</button>
+
+					<!-- Modal -->
+					<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+					  <div class="modal-dialog" role="document">
+					    <div class="modal-content">
+					      <div class="modal-header">
+					        <h5 class="modal-title" id="exampleModalLabel">Ajouter aux favoris</h5>
+					        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					          <span aria-hidden="true">&times;</span>
+					        </button>
+					      </div>
+					      <div class="modal-body">
+							  <p>Pays: <?= $row->countryName ?></p>
+  							  <p>Rubrique: <?= $row->categoryName ?></p>
+  							  <h2><?= $row->postTitle ?></h2>
+					      </div>
+					      <div class="modal-footer">
+					        <button type="button" id="modalCancel" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+					        <a  id="favorite" class="btn btn-success btn-lg " role="button" aria-pressed="true" href="<?= base_url('connexion/formulaire') ?>" data-postId="<?= $row->postId ?>" data-userId="<?php if(isset($_SESSION['userData']->userId)) {echo $_SESSION['userData']->userId;} ?>">Ajouter</a>
+					      </div>
+					    </div>
+					  </div>
+				  </div>
+				<!-- /modal -->
+
 				<p>Pays: <?= $row->countryName ?></p>
 				<p>Rubrique: <?= $row->categoryName ?></p>
 				<h2><?= $row->postTitle ?></h2>
+				<!-- compare le postId à chaque postId de la liste des revues qui sont dans les favoris. s'il y a un qui correspond alors on ajoute un petit coeur pour signifier que cet article fait déjà partie des favoris  -->
+				<?php foreach ($favoritesList as $row1):
+					if ($row->postId == $row1->postId) : ?>
+						<i class="fa fa-heart"></i>
+					<?php endIf; ?>
+				<?php endforeach; ?>
 				<p>Publié par:</p>
 				<p><img width="8%" src=<?= '/myProjet/webroot/images/usersAvatar/' . $row->writerAvatar ?> alt="avatar"></p>
 				<p><?= $row->writerFirstName." ".$row->writerLastName ?> le <?= $row->postPublishingDate ?></p>
