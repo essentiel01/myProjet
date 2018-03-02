c130d2fede2f493b9fb43a03fb152a237aaf76a4
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-03-02 03:23:42 +0100

    - chargment de la library pagination dans le constructeur. - inclusion de $data[favoris] dans la condition de verification d'authentification d'un utilisateur et de $data[error] dans leelse de la même condition. - ajout de $queryParams1 utilisé pour la function countFavoris. - ajout de laconfiguration de pagination. - inclusion de tout le bloc de code lié auxfavoris dans la condition if de verification d'authentification.

--- apprdp/controllers/Favorites.php
+++ apprdp/controllers/Favorites.php
@@ -9,6 +9,8 @@ class Favorites extends CI_Controller
 	{
 		parent::__construct();
 		$this->load->model('posts_model');
+		$this->load->library('pagination');
+
 	}
 
 	/**
@@ -16,19 +18,24 @@ class Favorites extends CI_Controller
 	 */
 	public function index()
 	{
-		//données à envoyer à la vue
-		$data['favoris'] = array(
-			'headerTitle' => 'Favoris'
-		);
-
 		if (isset($_SESSION['userData'])) {
+
+			//données à envoyer à la vue
+			$data['favoris'] = array(
+				'headerTitle' => 'Favoris'
+			);
+
 			//headerLogged
 			$this->load->view('templates/headerLogged', $data['favoris']);
 			//liste des favoris
 			$this->load->view('favorites/index', $data['favoris']);
 		} else {
+			//données à envoyer à la vue
+			$data['error'] = array(
+				'headerTitle' => 'Accès refusé'
+			);
 			//header
-			$this->load->view('templates/header', $data['favoris']);
+			$this->load->view('templates/header', $data['error']);
 			//page d'erreur
 			show_error('Vous n\'êtes pas connecté. Pour accéder à vos favoris veuillez vous connecter à votre espace personnel. Merci', 500, 'Accès refusé');
 		}
@@ -43,43 +50,99 @@ class Favorites extends CI_Controller
 	 */
 	public function postsFavorites()
 	{
-		$queryParams = array(
-			'select' => 'posts.postId, postTitle, postSlug, countryName, categoryName, postPublishingDate',
-
-			'join1' => 'posts',
-			'on1' => 'posts.postId = posts_favorites.postId',
-			'inner1' => 'inner',
-
-			'join2' => 'categories',
-			'on2' => 'categories.categoryId = posts.postCategory',
-			'inner2' => 'inner',
-
-			'join3' => 'countries',
-			'on3' => 'countries.countryId = posts.postCountry',
-			'inner3' => 'inner',
-
-			'where' => array('userId' => $_SESSION['userData']->userId)
-		);
-
-
-
-		//données à envoyer à la vue
-		$data['favoris'] = array(
-			'headerTitle' => 'Favoris',
-			'mainTitle' => 'Ma sélection de revue de presse',
-			'favoritesList' => $this->posts_model->getFavorites('posts_favorites', $queryParams)->result(),
-			'emptyFavoritesList' => 'Votre liste de favorisest vide'
-		);
-
-		//chargement des vues
 		if (isset($_SESSION['userData'])) {
+
+			//offset de la close limit
+			$start = $this->uri->segment(3,0);
+
+			//param^tres de la requête permettant de retourner le nombre de revues de presse favoris
+			$queryParams1 = array(
+				//clause where. permettant de selectionner toutes les revues favoris d'un utilsateur donné.
+				'where' => array('userId' => $_SESSION['userData']->userId)
+			);
+
+			//configuration de la pagination avec bootstrap
+			// url de base auquel va être ajouté le numero de la page
+			$config['base_url'] =  base_url('espace-personnel/mes-revues-de-presse-favoris/')  ;
+
+			// nombre total de revues de presse favoris pour un utilisateur donné
+			$config['total_rows'] = $this->posts_model->countFavorites('posts_favorites', $queryParams1);
+
+			// nombre d'articles par page
+			$config['per_page'] = 3;
+
+			// pour signifier que c'est le deuxième segment de l'url qui correspond au numéro dela page
+			$config['uri_segment'] = 3;
+
+			$config['full_tag_open'] = '<nav aria-label="..."><ul class="pagination pagination-lg">';
+			$config['full_tag_close'] = '</ul></nav>';
+
+			$config['cur_tag_open'] = '<li class="page-item active"><span class ="page-link">';
+			$config['cur_tag_close'] = '</span></li>';
+
+			$config['first_link'] = FALSE;
+			$config['last_link'] = FALSE;
+
+			$config['num_tag_open'] = '<li class="page-item "><span class ="page-link">';
+			$config['num_tag_close'] = '</span></li>';
+
+			$config['prev_tag_open'] = '<li>';
+			$config['prev_link'] = 'Précédent';
+			$config['prev_tag_close'] = '</li>';
+
+			$config['next_tag_open'] = '<li>';
+			$config['next_link'] = 'Suivant';
+			$config['next_tag_close'] = '</li>';
+
+			// initialisation de la pagination
+			$this->pagination->initialize($config);
+
+			//paramêtres de la requête permettant de selectioner toutes les revues de presse faavoris d'un utilisateur
+			$queryParams2 = array(
+				'select' => 'posts.postId, postTitle, postSlug, postAudio, countryName, categoryName, postPublishingDate',
+
+				'from' => 'posts_favorites',
+
+				'join1' => 'posts',
+				'on1' => 'posts.postId = posts_favorites.postId',
+				'inner1' => 'inner',
+
+				'join2' => 'categories',
+				'on2' => 'categories.categoryId = posts.postCategory',
+				'inner2' => 'inner',
+
+				'join3' => 'countries',
+				'on3' => 'countries.countryId = posts.postCountry',
+				'inner3' => 'inner',
+
+				'where' => array('userId' => $_SESSION['userData']->userId),
+
+				'limit' =>  $config['per_page'],
+				'offset' => $start,
+
+				'order' => 'categoryName '
+			);
+
+			//données à envoyer à la vue
+			$data['favoris'] = array(
+				'headerTitle' => 'Favoris',
+				'mainTitle' => 'Ma sélection de revue de presse',
+				'favoritesList' => $this->posts_model->getPosts($queryParams2)->result(),
+				'emptyFavoritesList' => 'Votre liste de favorisest vide'
+			);
+
+			//chargement des vues
 			//headerLogged
 			$this->load->view('templates/headerLogged', $data['favoris']);
 			//liste des favoris
 			$this->load->view('favorites/postsFavoritesView', $data['favoris']);
 		} else {
+			//données à envoyer à la vue
+			$data['error'] = array(
+				'headerTitle' => 'Accès refusé'
+			);
 			//header
-			$this->load->view('templates/header', $data['favoris']);
+			$this->load->view('templates/header', $data['error']);
 			//page d'erreur
 			show_error('Vous n\'êtes pas connecté. Pour accéder à vos favoris veuillez vous connecter à votre espace personnel. Merci', 500, 'Accès refusé');
 		}
