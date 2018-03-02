b1a1f84b7aff73a14fa2a3ef633de7d05612d38f
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-03-02 03:39:08 +0100

    - ajout d'un modal pour lire l'audio depuis la page favoris. - ajout de la pagination

--- apprdp/views/favorites/postsFavoritesView.php
+++ apprdp/views/favorites/postsFavoritesView.php
@@ -3,8 +3,7 @@
 		<h2><?= $mainTitle ?></h2>
 		<!-- si la liste des favoris n'est pas vide on l'affiche -->
 		<?php if(!empty($favoritesList)) {
-			 foreach ($favoritesList as $row) : ?>
-
+			foreach ($favoritesList as $row) : ?>
 				<form class="" method="post" action="<?= base_url('favorites/deletepost') ?>">
 					<input type="checkbox" name="favoriteCheckbox" value="">
 					<input type="hidden" name="postId" value="<?= $row->postId ?>">
@@ -13,11 +12,41 @@
 					<p>Titre: <?= $row->postTitle ?></p>
 					<div class="">
 						<a href="<?= base_url('culture/publication/' . $row->postId . '/' . $row->postSlug); ?>" class="btn btn-success btn-lg">Lire</a>
-						<a href="#" class="btn btn-success btn-lg">Ecouter</a>
+						<!-- Button trigger audioModal -->
+						<a href="#" class="btn btn-success btn-lg " role="button" data-toggle="modal" data-target="#audioModal<?= $row->postId ?>" aria-pressed="true">Ecouter</a>
 						<input type="submit"  class="btn btn-danger btn-lg" value="Supprimer" />
+					</div>
+					<!-- audioModal -->
+					<div class="modal fade" id="audioModal<?= $row->postId ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h5 class="modal-title" id="exampleModalLabel">Ecouter la revue</h5>
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+									</button>
+								</div>
+								<div class="modal-body">
+									<audio controls  src="<?= base_url('webroot/audio/' . $row->postAudio . '.ogg'); ?>"></audio>
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+									<a <?php if(!isset($_SESSION['userData']->userId)) {
+										echo 'href=' . base_url('connexion/formulaire');
+									} else {
+										echo 'href=' . base_url('webroot/audio/' . $row->postAudio . '.ogg') . ' download=' . $row->postAudio;
+									}
+									?> role="button" class="btn btn-primary" >
+									Télécharger
+									</a>
+								</div>
+							</div>
+						</div>
 					</div>
 				</form>
 			<?php endforeach ;
+			// pagination
+			echo $this->pagination->create_links(); 
 		} else {
 			// sinon on affiche un message de liste vide
 			if(isset ($emptyFavoritesList)) { echo '<p>' . $emptyFavoritesList . '</p>';}
