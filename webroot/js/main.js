8d14ce65c7f468f5d9ab1cfcae35080c030bfc0c
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-03-15 21:29:54 +0100

    création des events click sur le boutoncomment et le buton reply. création du timer setInterval sur la function getComment.

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
 	//event click du boutton favoris
 	$('#favorite').on('click', addFavorite);
+
+	//event click du boutton commenter
+	$('#btnComment').on('click', addComment);
+
+	//permet d'afficher les commentaires d'un article et d'actualiser les données chaque seconde
+	setInterval('getComments()', 1500);
+
+	//met le focus sur le champ commentaire lorsqu'on clique sur repondre
+	$('main').on('click', '.reply', replyComment);
+
+	//remet la valeur de l'input hidden contenant la valeur de l'id du commentaire parent àvide
+	// $('#comment').on('blur', function(){
+	// 	$('#parentCommentId').val('');
+	// });
 });
