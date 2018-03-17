8f18da2e35471f8959a763d7f893633a35e262b8
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-03-17 01:56:20 +0100

    - creation des event click sur lesbuttons addChronicFavotrites et addChronicComment - suppression desanciens et création de nouveau timer sur les functions getpostcomments et getchroniccomments.

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -1,18 +1,23 @@
 $(document).ready(function () {
-	//event click du boutton favoris
-	$('#favorite').on('click', addFavorite);
+	//event click du boutton ajout de favoris pour les revues
+	$('#addPostFavorite').on('click', addPostFavorite);
+
+	//event click du boutton  ajout de favoris pour les chroniques
+	$('#addChronicFavorite').on('click', addChronicFavorite);
 
 	//event click du boutton commenter
-	$('#btnComment').on('click', addComment);
+	$('#addPostComment').on('click', addPostComment);
+
+	//event click du boutton commenter
+	$('#addChronicComment').on('click', addChronicComment);
 
 	//permet d'afficher les commentaires d'un article et d'actualiser les données chaque seconde
-	setInterval('getComments()', 1500);
+	setInterval('getPostComments()', 1500);
+
+	//permet d'afficher les commentaires d'une chronique et d'actualiser les données chaque seconde
+	setInterval('getChronicComments()', 1500);
 
 	//met le focus sur le champ commentaire lorsqu'on clique sur repondre
 	$('main').on('click', '.reply', replyComment);
 
-	//remet la valeur de l'input hidden contenant la valeur de l'id du commentaire parent àvide
-	// $('#comment').on('blur', function(){
-	// 	$('#parentCommentId').val('');
-	// });
 });
