a27139ee7638ec90671525e71bdd5987682c3a42
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-03-17 01:25:41 +0100

    - la function index() devient postComments(). - remplacement du $_get par $_post dans la function postComments() et chronicComments(). - création de la variable $output qui est retournée à la requête ajax par les functions postComments() et chronicComments(). - création de la function chronicComments(). - addComment() devient addPostComment(). - création de function addChronicComment()

--- apprdp/controllers/Comments.php
+++ apprdp/controllers/Comments.php
@@ -10,10 +10,12 @@ class Comments extends CI_Controller
 		parent::__construct();
 		$this->load->model('posts_model');
 	}
+
+
 /**
  * permet d'afficher tous les commentaires d'un article via une requête ajax.
  */
-	public function index()
+	public function postComments()
 	{
 		//paramètres de la requête sql permettant de sélectioner les commentaires liés à un artice donné.
 		$queryParams = array(
@@ -23,7 +25,7 @@ class Comments extends CI_Controller
 			'on1' => 'posts_comments.userId = users.userId',
 			'inner1' => 'inner',
 
-			'where' => array('postId' => $_GET['postId']),
+			'where' => array('postId' => $_POST['postId']),
 
 			'order' => 'commentDate DESC'
 		);
@@ -43,14 +45,70 @@ class Comments extends CI_Controller
 			}
 		}
 
-		echo json_encode($comments);
-
+		//var_dump($comments);
+		$output = '';
+		foreach ($comments as $row) {
+			$output .= '<div><div> Par ' . $row->userFirstName . ' ' . $row->userLastName . ' le ' . $row->commentDate . '</div> <div>' . $row->commentContent . '</div><button type="button" class="reply btn btn-lg btn-primary" data-commentId=' . $row->commentId . '>Répondre</button></div>';
+			if (isset($row->children)) {
+				foreach ($row->children as $row) {
+					$output .= '<div style="margin-left: 40px;"><div> Par ' . $row->userFirstName . ' ' . $row->userLastName . ' le ' . $row->commentDate . '</div> <div>' . $row->commentContent . '</div></div>';
+				}
+			}
+		}
+		echo $output;
 	}
+
+
+
+	/**
+	 * permet d'afficher tous les commentaires d'une chronique via une requête ajax.
+	 */
+		public function chronicComments()
+		{
+			//paramètres de la requête sql permettant de sélectioner les commentaires liés à un artice donné.
+			$queryParams = array(
+				'select' => 'commentId, commentContent, parentCommentId, commentDate, userFirstName, userLastName, userAvatar',
+
+				'join1' => 'users',
+				'on1' => 'chronics_comments.userId = users.userId',
+				'inner1' => 'inner',
+
+				'where' => array('chronicId' => $_POST['chronicId']),
+
+				'order' => 'commentDate DESC'
+			);
+
+			$comments = $this->posts_model->getComments('chronics_comments', $queryParams)->result();
+
+			$commentById = [];
+
+			foreach ($comments as  $comment) {
+				$commentById[$comment->commentId] = $comment;
+			}
+
+			foreach ($comments as $key=>$comment) {
+				if ($comment->parentCommentId != 0){
+					$commentById[$comment->parentCommentId]->children[] = $comment;
+					unset($comments[$key]);
+				}
+			}
+			// var_dump($comments);
+			$output = '';
+			foreach ($comments as $row) {
+				$output .= '<div><div> Par ' . $row->userFirstName . ' ' . $row->userLastName . ' le ' . $row->commentDate . '</div> <div>' . $row->commentContent . '</div><button type="button" class="reply btn btn-lg btn-primary" data-commentId=' . $row->commentId . '>Répondre</button></div>';
+				if (isset($row->children)) {
+					foreach ($row->children as $row) {
+						$output .= '<div style="margin-left: 40px;"><div> Par ' . $row->userFirstName . ' ' . $row->userLastName . ' le ' . $row->commentDate . '</div> <div>' . $row->commentContent . '</div></div>';
+					}
+				}
+			}
+			echo $output;
+		}
 
 	/**
 	 * traitement des données envoyées par la requête ajaxet insertion du commentaire dans la base de données.
 	 */
-	public function addComment()
+	public function addPostComment()
 	{
 		if (isset($_POST['comment']) && trim($_POST['comment'], '') != ''){
 			$dataToSave = array(
@@ -61,6 +119,25 @@ class Comments extends CI_Controller
 			);
 
 			$this->posts_model->saveNew('posts_comments', $dataToSave);
+		}
+
+	}
+
+
+	/**
+	 * traitement des données envoyées par la requête ajaxet insertion du commentaire des chroniques dans la base de données.
+	 */
+	public function addChronicComment()
+	{
+		if (isset($_POST['comment']) && trim($_POST['comment'], '') != ''){
+			$dataToSave = array(
+				'commentContent' => htmlentities($_POST['comment']),
+				'parentCommentId' => $_POST['parentCommentId'],
+				'chronicId' => $_POST['chronicId'],
+				'userId' => $_POST['userId']
+			);
+
+			$this->posts_model->saveNew('chronics_comments', $dataToSave);
 		}
 
 	}
