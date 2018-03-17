5139e951531504fbcfd44c225f0c4543d56e97f8
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-03-17 01:53:23 +0100

    addFavorites() devient addPostFavorites() addComment devient addPostComment() - creation de addChronicComment et de addChronicFazvorite - suppression des anciennes et création de nouvelles function getPostComments() etgetChronicComment()

--- webroot/js/ajax.js
+++ webroot/js/ajax.js
@@ -2,7 +2,7 @@
  * envoie les données de la revue à ajouter à la liste des favoris via la methode post de ajax
  * @param Event e événnement click
  */
-function addFavorite(e)
+function addPostFavorite(e)
 {
 	if ($(this).attr('data-userId') != '') {
 
@@ -20,18 +20,40 @@ function addFavorite(e)
 	}
 }
 
+
+/**
+ * envoie les données de la revue à ajouter à la liste des favoris via la methode post de ajax
+ * @param Event e événnement click
+ */
+function addChronicFavorite(e)
+{
+	if ($(this).attr('data-userId') != '') {
+
+		e.preventDefault();
+		$.post(
+			"/myProjet/favorites/addChronic",
+			{
+				chronicId : $(this).attr('data-chronicId'),
+				userId : $(this).attr('data-userId')
+			}
+		).done(function ()
+		{
+			$('#modalCancel').trigger('click');
+		});
+	}
+}
 /**
  * ajoute un commentaire
  * @param Event e evennement click
  */
-function addComment(e)
+function addPostComment(e)
 {
 	// on vérifie si quelqu'un est connectée
 	if ($(this).attr('data-userId') != '') {
 		//si quelqu'un est connecté on désactive le lien href puis on exécute le requête ajax
 		e.preventDefault();
 		$.post(
-			"/myProjet/comments/addComment",
+			"/myProjet/comments/addPostComment",
 			{
 				postId : $(this).attr('data-postId'),
 				userId : $(this).attr('data-userId'),
@@ -48,41 +70,48 @@ function addComment(e)
 }
 
 /**
+ * ajoute un commentaire
+ * @param Event e evennement click
+ */
+function addChronicComment(e)
+{
+	// on vérifie si quelqu'un est connectée
+	if ($(this).attr('data-userId') != '') {
+		//si quelqu'un est connecté on désactive le lien href puis on exécute le requête ajax
+		e.preventDefault();
+		$.post(
+			"/myProjet/comments/addChronicComment",
+			{
+				chronicId : $(this).attr('data-chronicId'),
+				userId : $(this).attr('data-userId'),
+				comment: $('#comment').val(),
+				parentCommentId: $('#parentCommentId').val()
+			}
+		).done(function ()
+		{
+			// en cas de succès de la requête on vide le champ textarea du commenataire puis on réinitialise le parentCommentId à une valeur vide.
+			$('#comment').val('');
+			$('#parentCommentId').val('');
+		});
+	}
+}
+
+/**
  * requête ajax permetttant d'afficher la liste des commentaires
  */
-function getComments()
-{
-	$.get(
-		"/myProjet/comments/index", {postId: $('#btnComment').attr('data-postId')}
-	).done(function (data) {
-		//on efface tous les commentaires et on les réafiche chaque 1500ms
-		$('#displayComments').empty();
-		//conversion des de data au format json
-		data = JSON.parse(data);
 
-		// //on recupère chaque commentaire dans la variable eachComment puis on l'affiche dans le bloc displayComment
-		$.each(data, function (index){
-			var eachComment = $('<div class="eachComment panel ">').html(
-				'<div class="commentAuthor">Par ' + data[index].userFirstName + ' ' + data[index].userLastName + ' le ' + data[index].commentDate + '</div>'
-				+
-				'<div class="commentContent jumbotron jumbotron-fluid">' + data[index].commentContent + '</div>'
-				+
-				'<button type="button" class="reply btn btn-lg btn-primary" data-commentId=' + data[index].commentId + '>Répondre</button>'
-			);
-			//on vérifie si le commentaire à une réponse. si oui on affiche lesrèponses à la suite du commentaire
-			if (data[index].children != '') {
-				$.each(data[index].children, function(i){
-					var eachReply = $('<div class="eachComment" style="margin-left: 40px">').html(
-					'<div class="commentAuthor">Par ' + data[index].children[i].userFirstName + ' ' + data[index].children[i].userLastName + ' le ' + data[index].children[i].commentDate + '</div>'
-					+
-					'<div class="commentContent jumbotron jumbotron-fluid">' + data[index].children[i].commentContent + '</div>');
-					$(eachComment).append(eachReply);
-				});
-			 }
-			$('#displayComments').append(eachComment);
-		});
-	});
+ function getPostComments() {
+ 	$('#displayPostComments').load("/myProjet/comments/postComments", {postId: $('#addPostComment').attr('data-postId')});
+ }
+
+
+/**
+ * requête ajax permetttant d'afficher la liste des commentaires des chroniques
+ */
+function getChronicComments() {
+	$('#displayChronicComments').load("/myProjet/comments/chronicComments", {chronicId: $('#addChronicComment').attr('data-chronicId')});
 }
+
 
 /**
  * permet de répondre à un commentaire
