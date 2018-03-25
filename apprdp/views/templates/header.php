cab87ef4734adcdff3ac03b4fb5376cbcc5a6b2a
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-03-26 01:29:44 +0200

    ajout de la balise meta viewport réorganisation du code html

--- apprdp/views/templates/header.php
+++ apprdp/views/templates/header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, user-scalable=no">
 	<title><?= $headerTitle ?></title>
 	<!-- bootstrap css cdn -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"/>
@@ -30,46 +31,58 @@
 		<div class="topBar">
 			<!-- social icons -->
 			<div class="socialIcons">
-				<i class="fa fa-twitter-square" aria-hidden="true"></i>
-				<i class="fa fa-facebook-official" aria-hidden="true"></i>
-				<i class="fa fa-envelope" aria-hidden="true"></i>
+				<i class="fa fa-twitter-square twitter" aria-hidden="true"></i>
+				<i class="fa fa-facebook-official facebook" aria-hidden="true"></i>
+				<i class="fa fa-envelope enveloppe" aria-hidden="true"></i>
 			</div>
 			<!-- suscribe and sign in buttons -->
 			<div class="buttons">
-				<a href=<?= base_url('inscription/formulaire'); ?> class="btn btn-primary btn-xs">S'inscrire</a>
-				<a href=<?= base_url('connexion/formulaire'); ?> class="btn btn-success btn-xs">Se connecter</a>
+				<a href=<?= base_url('inscription/formulaire'); ?> class="btn  btn-xs">S'inscrire</a>
+				<a href=<?= base_url('connexion/formulaire'); ?> class="btn  btn-xs">Se connecter</a>
 			</div>
 		</div>
 		<!-- page title -->
-		<div class="titleWrapper container">
+		<div class="titleAndSubtitle content">
 			<!-- title -->
 			<h1>A LA CARTE</h1>
 			<!-- subtitle -->
 			<p class="subTitle">Revue de presse écrite et audio</p>
 		</div>
 		<!-- live infos panel-->
-		<div class="flashInfos container">
+		<div class="flashInfos content">
 			<p>Contenu de l'info</p>
 		</div>
 		<!-- nav and search form -->
-		<div class="navWrapper container">
+		<div class="navParent content">
+
+			<div class="menuBtnMobile">
+				<i class="fa fa-bars hamburger" id="hamburger"></i>
+				<!-- search form -->
+				<form class="searchMobile hide input-group mb-3" id="searchMobile" action="">
+					<input class="form-control-sm" type="text" placeholder="rechercher">
+					<div class="input-group-append btn-search">
+						<input type="submit" class="btn-primary btn-sm"  value="Ok"/>
+					</div>
+				</form>
+				<i class="fa fa-search loop" id="loop" aria-hidden="true"></i>
+			</div>
 			<!-- nav -->
 			<nav>
-				<ul class="nav nav-tab">
-					<li class="nav-item">
-						<a class="nav-link" href=""><i class="fa fa-home" aria-hidden="true"></i></a>
+				<ul class="hide">
+					<li class="">
+						<a class="" href=""><i class="fa fa-home" aria-hidden="true"></i></a>
 					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="">Politique</a>
+					<li class="">
+						<a class="" href="">Politique</a>
 					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="">Economie</a>
+					<li class="">
+						<a class="" href="">Economie</a>
 					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="" >Sport</a>
+					<li class="">
+						<a class="" href="" >Sport</a>
 					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="<?= base_url('culture') ?>">Culture</a>
+					<li class="">
+						<a class="" href="<?= base_url('culture') ?>">Culture</a>
 					</li>
 				</ul>
 			</nav>
