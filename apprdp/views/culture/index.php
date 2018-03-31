efc44536fdf1cc903fa0d579d992a2583d802752
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-03-31 22:00:02 +0200

    style de la page index. ajout de class css et réorganisation du code html

--- apprdp/views/culture/index.php
+++ apprdp/views/culture/index.php
@@ -1,30 +1,34 @@
-<div class="container">
+<div class="content">
 	<!-- main -->
-	<main  class="mainWrapper">
-		<section class="post">
-			<!-- titre de la page -->
-			<h2><?= $mainTitle ?></h2>
+	<main  class="main">
+		<!-- titre de la page -->
+		<h2><?= $mainTitle ?></h2>
+		<section class="posts">
 			<?php foreach ($result as $row):?>
 				<!-- infos de chaque post -->
-				<div class="">
+				<div class="post">
 					<p><?= $row->countryName ?></p>
-					<p>Rubrique: <?= $row->categoryName ?></p>
-					<p>Titre: <?= $row->postTitle ?></p>
-					<!-- compare le postId à chaque postId de la liste des revues qui sont dans les favoris. s'il y a un qui correspond alors on ajoute un petit coeur pour signifier que cet article fait déjà partie des favoris  -->
-					<?php if (isset($favoritesList)) {
-						foreach ($favoritesList as $row1):
-							if ($row->postId == $row1->postId) : ?>
-							<i class="fa fa-heart"></i>
-							<?php endIf; ?>
-						<?php endforeach;
-					} ?>
-					<p>Publié par:</p>
-					<img width="8%" src="<?= base_url('webroot/images/usersAvatar/' . $row->writerAvatar) ?>"  alt="avatar">
-					<p><?= $row->writerFirstName . " " . $row->writerLastName ?></p>
-					<p>le <strong><?= $row->postDate ?></strong></p>
-					<a href=<?= base_url('culture/publication/' . $row->postId . '/' . $row->postSlug); ?> class="btn btn-success btn-lg " role="button" aria-pressed="true">Lire</a>
-					<!-- Button trigger audioModal -->
-					<a href="#" class="btn btn-success btn-lg " role="button" data-toggle="modal" data-target="#audioModal<?= $row->postId ?>" aria-pressed="true">Ecouter</a>
+					<!-- <p>Rubrique: <?= $row->categoryName ?></p> -->
+					<!-- icone favoris -->
+					<h3><?= $row->postTitle ?>
+						<?php if (isset($favoritesList)) {
+							foreach ($favoritesList as $row1):
+								if ($row->postId == $row1->postId) : ?>
+								<i class="fa fa-heart fa-coeur"></i>
+								<?php endIf; ?>
+							<?php endforeach;
+						} ?>
+					</h3>
+
+					<p class="auteur"><strong>Par:</strong> <?= $row->writerFirstName . " " . $row->writerLastName ?> <strong>le</strong> <?= $row->postDate ?></p>
+					<!-- <div class="auteur"> -->
+						<!-- <img width="8%" src="<?= base_url('webroot/images/usersAvatar/' . $row->writerAvatar) ?>"  alt="avatar"> -->
+					<!-- </div> -->
+					<div class="btnLecture">
+						<a href=<?= base_url('culture/publication/' . $row->postId . '/' . $row->postSlug); ?> class="btn btn-success btn-lg " role="button" aria-pressed="true">Lire</a>
+						<!-- Button trigger audioModal -->
+						<a href="#" class="btn btn-success btn-lg " role="button" data-toggle="modal" data-target="#audioModal<?= $row->postId ?>" aria-pressed="true">Ecouter</a>
+					</div>
 
 
 					<!-- audioModal -->
@@ -62,22 +66,9 @@
 		<!-- /pagination -->
 	</section>
 </main><!--
---><aside class="asideWrapper">
-<!-- chronique -->
-<section class="chronic">
-	<?php foreach ($chronic as $row) : ?>
-		<h2><span><?= $row->countryName ?>: </span><?= $row->chronicTitle ?></h2>
-		<p>Auteur</p>
-		<img src=<?= base_url('webroot/images/usersAvatar/' . $row->writerAvatar) ?> alt="avatar">
-		<p><?= $row->writerFirstName. ' ' .$row->writerLastName ?></p>
-		<p>Le <?= $row->chronicDate ?></p>
-		<p><?= substr($row->chronicContent, 0, 500); ?><a href=<?= base_url('culture/chronique/' . $row->chronicId . '/' . $row->chronicSlug) ?>>...Lire plus</a></p>
-	<?php endforeach; ?>
-</section>
-<!-- /chronique -->
-
-<!--  carousel -->
-<section class="slide">
+--><aside class="aside">
+<!--  carousel pour mobile-->
+<section class="sliderMobile">
 	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
 		<ol class="carousel-indicators">
 			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -86,13 +77,58 @@
 		</ol>
 		<div class="carousel-inner">
 			<?php foreach ($slides as $row): ?>
-				<div class="carousel-item <?php if (substr($row->slideName, -1) == 1){echo 'active';} ?>">
-					<img class="d-block w-100" src="<?= base_url('webroot/images/carousel/' . $row->slideName . '.jpg') ?>" alt="<?= $row->slideName ?>">
-					<div class="carousel-caption d-none d-md-block">
-					    <h5><?= $row->slideLabel ?></h5>
-					    <p><?= $row->slideDescription ?></p>
-					 </div>
-				</div>
+				<a href="#">
+					<div class="carousel-item <?php if (substr($row->slideName, -1) == 1){echo 'active';} ?>">
+						<img class="d-block w-100" src="<?= base_url('webroot/images/carousel/' . $row->slideName . '.jpg') ?>" alt="<?= $row->slideName ?>">
+						<div class="carousel-caption d-none d-md-block">
+						    <h5><?= $row->slideLabel ?></h5>
+						    <p><?= $row->slideDescription ?></p>
+						 </div>
+					</div>
+				</a>
+			<?php endforeach; ?>
+
+			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+	</div>
+</section>
+<!-- chronique -->
+<section class="chronic">
+	<?php foreach ($chronic as $row) : ?>
+		<h2><?= $row->countryName ?>: <?= $row->chronicTitle ?></h2>
+		<p class="auteur"><strong>Par:</strong> <?= $row->writerFirstName. ' ' .$row->writerLastName ?> <strong>le</strong> <?= $row->chronicDate ?></p>
+		<!-- <img src=<?= base_url('webroot/images/usersAvatar/' . $row->writerAvatar) ?> alt="avatar"> -->
+		<p class="chronicContent"><?= substr($row->chronicContent, 0, 300); ?>...<a class="readMore btn btn-success btn-lg btn-xs" href=<?= base_url('culture/chronique/' . $row->chronicId . '/' . $row->chronicSlug) ?>>Lire plus</a></p>
+	<?php endforeach; ?>
+</section>
+<!-- /chronique -->
+
+<!--  carousel -->
+<section class="slider">
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner">
+			<?php foreach ($slides as $row): ?>
+				<a href="#">
+					<div class="carousel-item <?php if (substr($row->slideName, -1) == 1){echo 'active';} ?>">
+						<img class="d-block w-100" src="<?= base_url('webroot/images/carousel/' . $row->slideName . '.jpg') ?>" alt="<?= $row->slideName ?>">
+						<div class="carousel-caption d-none d-md-block">
+						    <h5><?= $row->slideLabel ?></h5>
+						    <p><?= $row->slideDescription ?></p>
+						 </div>
+					</div>
+				</a>
 			<?php endforeach; ?>
 
 			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -109,7 +145,7 @@
 <!-- archive -->
 <section>
 	<div class="archive">
-		<h3>Nos archives</h3>
+		<h2>Nos archives</h2>
 		<ul class="list-group">
 			<a href="<?= base_url('revues-de-presse/archive') ?>">
 				<li class="list-group-item d-flex justify-content-between align-items-center">
@@ -122,6 +158,17 @@
 					Toutes nos chroniques
 					<span class="badge badge-primary badge-pill"><?= $allChronicsCount ?></span>
 				</li>
+			</a>
+		</ul>
+	</div>
+</section>
+<!-- décodeurs de l'actualité -->
+<section>
+	<div class="decodeur">
+		<h2>Les décodeurs de l'actu</h2>
+		<ul class="list-group">
+			<a href="#">
+				<li class="list-group-item d-flex justify-content-between align-items-center">Lorem ipsum dolor sit amet, consectetur adipisicing</li>
 			</a>
 		</ul>
 	</div>
