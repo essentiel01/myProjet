b6b4a75a875aef2e0d56026d43fd6a8d53cf4f38
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-04-02 00:31:31 +0200

    style css ajout des class css

--- apprdp/views/culture/singleView.php
+++ apprdp/views/culture/singleView.php
@@ -1,28 +1,30 @@
-<div class="container">
-	<main class="mainWrapper">
+<div class="content">
+	<main class="main">
 		<?php if (!empty($post)) {
 			 	foreach ($post as $row) : ?>
 				<!-- Button trigger favorisModal -->
-				<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#favorisModal<?= $row->postId ?>">
-					Favoris
-				</button>
+				<div class="favoris">
+					<button type="button" class="btn btn-favoris" data-toggle="modal" data-target="#favorisModal<?= $row->postId ?>">
+						Favoris
+					</button>
+				</div>
 
 				<!-- favorisModal -->
-				<div class="modal fade" id="favorisModal<?= $row->postId ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+				<div class="modal fade fModal" id="favorisModal<?= $row->postId ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 					<div class="modal-dialog" role="document">
 						<div class="modal-content">
-							<div class="modal-header">
+							<div class="modal-header header">
 								<h5 class="modal-title" id="exampleModalLabel">Ajouter aux favoris</h5>
 								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 									<span aria-hidden="true">&times;</span>
 								</button>
 							</div>
-							<div class="modal-body">
+							<div class="modal-body body">
 								<p>Pays: <?= $row->countryName ?></p>
 								<p>Rubrique: <?= $row->categoryName ?></p>
 								<h2><?= $row->postTitle ?></h2>
 							</div>
-							<div class="modal-footer">
+							<div class="modal-footer footer">
 								<button type="button" id="modalCancel" class="btn btn-secondary btn-lg" data-dismiss="modal">Annuler</button>
 								<a  id="addPostFavorite" class="btn btn-success btn-lg " role="button" aria-pressed="true" href="<?= base_url('connexion/formulaire') ?>" data-postId="<?= $row->postId ?>" data-userId="<?php if(isset($_SESSION['userData']->userId)) {echo $_SESSION['userData']->userId;} ?>">Ajouter</a>
 							</div>
@@ -31,34 +33,41 @@
 				</div>
 				<!-- /modal -->
 
-				<p>Pays: <?= $row->countryName ?></p>
-				<p>Rubrique: <?= $row->categoryName ?></p>
-				<!-- icone favoris -->
-				<h2><?= $row->postTitle ?>
-					<?php if (isset($favoritesList)) {
-						foreach ($favoritesList as $row1):
-							if ($row->postId == $row1->postId) : ?>
-							<i class="fa fa-heart fa-coeur"></i>
-							<?php endIf; ?>
-						<?php endforeach;
-					} ?>
-				</h2>
+				<div class="singlePost">
+					<p><strong>Pays:</strong> <?= $row->countryName ?></p>
+					<p><strong>Rubrique:</strong> <?= $row->categoryName ?></p>
+					<!-- icone favoris -->
+					<h2><?= $row->postTitle ?>
+						<?php if (isset($favoritesList)) {
+							foreach ($favoritesList as $row1):
+								if ($row->postId == $row1->postId) : ?>
+								<i class="fa fa-heart fa-coeur"></i>
+								<?php endIf; ?>
+							<?php endforeach;
+						} ?>
+					</h2>
 
-				<p>Publié par:</p>
-				<p><img width="8%" src=<?= '/myProjet/webroot/images/usersAvatar/' . $row->writerAvatar ?> alt="avatar"></p>
-				<p><?= $row->writerFirstName." ".$row->writerLastName ?> le <?= $row->postDate ?></p>
-				<p><?= $row->postContent ?></p>
-				<h2>Sources de la revue</h2>
-				<p><?= $row->postSource ?></p>
+					<div class="auteur">
+						<p><strong>Par</strong></p>
+						<p><img class="avatar"  src=<?= '/myProjet/webroot/images/usersAvatar/' . $row->writerAvatar ?> alt="avatar"></p>
+						<p><?= $row->writerFirstName." ".$row->writerLastName ?> <strong>le</strong> <?= $row->postDate ?></p>
+					</div>
+					<p class="postContent"><?= $row->postContent ?></p>
+					<div class="source">
+						<h2>Sources de la revue</h2>
+						<p class="sourceItems"><?= $row->postSource ?></p>
+					</div>
+				</div>
 
 				<!-- formulaire de commentaire -->
-				<form class="" action="" method="post">
+				<form class="comment" action="" method="post">
 					<label for="comment">Ajouter un commentaire</label>
 					<textarea id="comment" name="comment" rows="2" cols="70"></textarea>
 					<input type="hidden" id="parentCommentId" name="parentCommentId" value="0">
 				</form>
 				<a id="addPostComment" class="btn btn-lg btn-primary" href="<?= base_url('connexion/formulaire') ?>" data-postId="<?= $row->postId ?>" data-userId="<?php if(isset($_SESSION['userData']->userId)) {echo $_SESSION['userData']->userId;} ?>">Valider</a>
 				<!-- liste des commentaires -->
+				<h4 class="commentTitle">Commentaires</h4>
 				<div id="displayPostComments" class="displayComments">
 
 				</div>
