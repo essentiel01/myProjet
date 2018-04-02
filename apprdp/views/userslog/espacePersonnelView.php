819364bade685bdc870604d0ab29ec2b1c7b3e9e
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-04-02 23:46:47 +0200

    ajout de class css

--- apprdp/views/userslog/espacePersonnelView.php
+++ apprdp/views/userslog/espacePersonnelView.php
@@ -1,6 +1,8 @@
-<div class="container">
-	<h2>Bienvenue dans votre espace personnel</h2>
-	<a href="#">Mes infromations</a>
-	<a href="#">Mes revues favoris</a>
-	<a href="#">Mes chroniques favoris</a>
+<div class="content">
+	<div class="espace">
+		<h2>Bienvenue dans votre espace personnel</h2>
+		<a href="#">Mes infromations</a>
+		<a href="#">Mes revues favoris</a>
+		<a href="#">Mes chroniques favoris</a>
+	</div>
 </div>
