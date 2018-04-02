7b6fd1630a99e2fbeedc45745f455699b4baacbd
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-04-02 14:50:17 +0200

    ajout de class css sur les span de form_errors

--- apprdp/views/usersRegister/registerFormView.php
+++ apprdp/views/usersRegister/registerFormView.php
@@ -7,52 +7,52 @@
 				<div class="col-lg-5 col-sm-8">
 					<input type="text" class="form-control" id="firstName" name="firstName" value="<?php if(form_error('firstName') == NULL){echo set_value('firstName');} ?>" placeholder="Prénom">
 				</div>
-				<span><?= form_error('firstName'); ?></span>
+				<span class="formError"><?= form_error('firstName'); ?></span>
 			</div>
 			<div class="form-group row">
 				<label for="lastName" class="col-lg-2 col-sm-2 col-form-label">Nom</label>
 				<div class="col-lg-5 col-sm-8">
 					<input type="text" class="form-control" id="lastName" name="lastName" value="<?php if(form_error('lastName') == NULL){echo set_value('lastName');} ?>" placeholder="Nom">
 				</div>
-				<span><?= form_error('lastName'); ?></span>
+				<span class="formError"><?= form_error('lastName'); ?></span>
 			</div>
 			<div class="form-group row">
 				<label for="login" class="col-lg-2 col-sm-2 col-form-label">Identifiant</label>
 				<div class="col-lg-5 col-sm-8">
 					<input type="text" class="form-control" id="login" name="login" value="<?php if(form_error('login') == NULL){echo set_value('login');} ?>" placeholder="Identifiant">
 				</div>
-				<span><?= form_error('login'); ?></span>
+				<span class="formError"><?= form_error('login'); ?></span>
 			</div>
 			<div class="form-group row">
 				<label for="email" class="col-lg-2 col-sm-2 col-form-label">Email</label>
 				<div class="col-lg-5 col-sm-8">
 					<input type="email" class="form-control" id="email" name="email" value="<?php if(form_error('email') == NULL){echo set_value('email');} ?>" placeholder="Email">
 				</div>
-				<span><?= form_error('email'); ?></span>
+				<span class="formError"><?= form_error('email'); ?></span>
 			</div>
 			<div class="form-group row">
 				<label for="password" class="col-lg-2 col-sm-2 col-form-label">Mot de passe</label>
 				<div class="col-lg-5 col-sm-8">
 					<input type="password" class="form-control" id="password" name="password" placeholder="Mot de passe">
 				</div>
-				<span><?= form_error('password'); ?></span>
+				<span class="formError"><?= form_error('password'); ?></span>
 			</div>
 			<div class="form-group row">
 				<label for="passconf" class="col-lg-2 col-sm-2 col-form-label">Confirmer le mot de passe</label>
 				<div class="col-lg-5 col-sm-8">
 					<input type="password" class="form-control" name="passconf" id="passconf" placeholder="Confirmation du mot de passe">
 				</div>
-				<span><?= form_error('passconf'); ?></span>
+				<span class="formError"><?= form_error('passconf'); ?></span>
 			</div>
 			<div class="form-group row">
 				<label for="country" class="col-lg-2 col-sm-2 col-form-label">Pays</label>
 				<div class="col-lg-5 col-sm-8">
 					<input type="text" class="form-control" id="country" name="country" value="<?php if(form_error('country') == NULL){echo set_value('country');} ?>" placeholder="Pays">
 				</div>
-				<span><?= form_error('country'); ?></span>
+				<span class="formError"><?= form_error('country'); ?></span>
 			</div>
 			<div class="form-group row">
-			    <div class="col-lg-5 col-sm-8">
+			    <div class="col-lg-5 col-sm-8 btnRegister">
 			    	<input type="submit" class="btn btn-primary" value ="S'inscrire">
 			    </div>
 		  	</div>
