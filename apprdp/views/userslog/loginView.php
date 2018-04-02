80fad858b72e4ea7a4623a932867eb6bdc8516a9
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-04-02 22:35:15 +0200

    ajout de classe css

--- apprdp/views/userslog/loginView.php
+++ apprdp/views/userslog/loginView.php
@@ -1,34 +1,34 @@
-<div class="container">
+<div class="content">
 	<main>
-		<h2>Connectez-vous à votre espace personnel pour profiter de toutes les avantages du site</h2>
+		<h2 class="formTitle">Connectez-vous à votre espace personnel</h2>
 		<!-- message d'erreur en cas d'identifiant ou de mot de passe incorrecte -->
 		<p><?php if(isset($loginError)){echo $loginError;} ?></p>
 		<!-- formulaire de connexion -->
-		<form class="registerForm" action=<?= base_url('connexion-reussie') ?> method="post">
+		<form class="form" action=<?= base_url('connexion-reussie') ?> method="post">
 			<div class="form-group row">
 				<label for="email" class="col-lg-2 col-sm-2 col-form-label">Email</label>
 				<div class="col-lg-5 col-sm-8">
 					<input type="email" class="form-control" id="email" name="email" value="<?php if(form_error('email') == NULL){echo set_value('email');} ?>" placeholder="Email">
 				</div>
-				<span><?= form_error('email'); ?></span>
+				<span class="formError"><?= form_error('email'); ?></span>
 			</div>
 			<div class="form-group row">
 				<label for="password" class="col-lg-2 col-sm-2 col-form-label">Mot de passe</label>
 				<div class="col-lg-5 col-sm-8">
 					<input type="password" class="form-control" id="password" name="password" placeholder="Mot de passe">
 				</div>
-				<span><?= form_error('password'); ?></span>
+				<span class="formError"><?= form_error('password'); ?></span>
 			</div>
 
 			<div class="form-group row">
-			    <div class="col-lg-5 col-sm-8">
+			    <div class="col-lg-5 col-sm-8 btnRegister">
 			    	<input type="submit" class="btn btn-primary" value ="Se connecter">
 			    </div>
 		  	</div>
 			<!-- liens pour mot de passe oublié et création de compte -->
-			<div class="">
+			<div class="formLink">
 				<a href="#">Mot de passe oublié?</a>
-				<a href="<?= base_url('inscription/formulaire') ?>">Je crée mon compte en quelques clics</a>
+				<a href="<?= base_url('inscription/formulaire') ?>">Je crée mon compte</a>
 			</div>
 		</form>
 	</main>
