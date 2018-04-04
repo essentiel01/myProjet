00fd6669e00788937bf96a80f513de9f32a164f7
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-04-04 22:42:34 +0200

    ajout de classe css suppression du modal audio

--- apprdp/views/favorites/chronicsFavoritesView.php
+++ apprdp/views/favorites/chronicsFavoritesView.php
@@ -1,56 +1,26 @@
 <div class="container">
-	<main>
-		<h2><?= $mainTitle ?></h2>
-		<!-- si la liste des favoris n'est pas vide on l'affiche -->
-		<?php if(!empty($favoritesList)) {
-			foreach ($favoritesList as $row) : ?>
-				<form class="" method="post" action="<?= base_url('favorites/deletechronic') ?>">
-					<input type="checkbox" name="favoriteCheckbox" value="">
-					<input type="hidden" name="chronicId" value="<?= $row->chronicId ?>">
-					<p><?= $row->countryName ?></p>
-					<p>Rubrique: <?= $row->categoryName ?></p>
-					<p>Titre: <?= $row->chronicTitle ?></p>
-					<div class="">
-						<a href="<?= base_url('culture/publication/' . $row->chronicId . '/' . $row->chronicSlug); ?>" class="btn btn-success btn-lg">Lire</a>
-						<!-- Button trigger audioModal -->
-						<!-- <a href="#" class="btn btn-success btn-lg " role="button" data-toggle="modal" data-target="#audioModal<?= $row->chronicId ?>" aria-pressed="true">Ecouter</a> -->
-						<input type="submit"  class="btn btn-danger btn-lg" value="Supprimer" />
-					</div>
-					<!-- audioModal -->
-					<!-- <div class="modal fade" id="audioModal<?= $row->chronicId ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-						<div class="modal-dialog" role="document">
-							<div class="modal-content">
-								<div class="modal-header">
-									<h5 class="modal-title" id="exampleModalLabel">Ecouter la chronique</h5>
-									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-										<span aria-hidden="true">&times;</span>
-									</button>
-								</div>
-								<div class="modal-body">
-									<audio controls  src="<?= base_url('webroot/audio/' . $row->chronicAudio . '.ogg'); ?>"></audio>
-								</div>
-								<div class="modal-footer">
-									<button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-									<a <?php if(!isset($_SESSION['userData']->userId)) {
-										echo 'href=' . base_url('connexion/formulaire');
-									} else {
-										echo 'href=' . base_url('webroot/audio/' . $row->chronicAudio . '.ogg') . ' download=' . $row->chronicAudio;
-									}
-									?> role="button" class="btn btn-primary" >
-									Télécharger
-									</a>
-								</div>
-							</div>
+	<main class="main">
+		<section class="posts">
+			<h2><?= $mainTitle ?></h2>
+			<?php if(!empty($favoritesList)) {
+				foreach ($favoritesList as $row) : ?>
+					<form class="post" method="post" action="<?= base_url('favorites/deletechronic') ?>">
+						<input type="checkbox" name="favoriteCheckbox" value="">
+						<input type="hidden" name="chronicId" value="<?= $row->chronicId ?>">
+						<p><strong>Pays: </strong><?= $row->countryName ?></p>
+						<p><strong>Rubrique: </strong><?= $row->categoryName ?></p>
+						<p><strong>Titre: </strong><?= $row->chronicTitle ?></p>
+						<div class="btnLecture">
+							<a href="<?= base_url('culture/publication/' . $row->chronicId . '/' . $row->chronicSlug); ?>" class="btn btn-success btn-lg btn-sm">Lire</a>
+							<input type="submit"  class="btn btn-danger btn-lg btn-sm" value="Supprimer" />
 						</div>
-					</div> -->
-				</form>
-			<?php endforeach ;
-			// pagination
-			echo $this->pagination->create_links();
-		} else {
-			// sinon on affiche un message de liste vide
-			if(isset ($emptyFavoritesList)) { echo '<p>' . $emptyFavoritesList . '</p>';}
-		} ?>
-
+					</form>
+				<?php endforeach ;
+				// pagination
+				echo $this->pagination->create_links();
+			} else {
+				if(isset ($emptyFavoritesList)) { echo '<p>' . $emptyFavoritesList . '</p>';}
+			} ?>
+		</section>
 	</main>
 </div>
