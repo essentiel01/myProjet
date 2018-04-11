ab661779d294c5d0996da8dea7cb834d233430a4
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-04-11 23:33:50 +0200

    ajout des liens vers les favoris

--- apprdp/views/userslog/espacePersonnelView.php
+++ apprdp/views/userslog/espacePersonnelView.php
@@ -2,7 +2,7 @@
 	<div class="espace">
 		<h2>Bienvenue dans votre espace personnel</h2>
 		<a href="#">Mes infromations</a>
-		<a href="#">Mes revues favoris</a>
-		<a href="#">Mes chroniques favoris</a>
+		<a href="<?= base_url('espace-personnel/mes-revues-de-presse-favoris') ?>">Mes revues de presse favoris</a>
+		<a href="<?= base_url('espace-personnel/mes-chroniques-favoris') ?>">Mes chroniques favoris</a>
 	</div>
 </div>
