df56982b26463fcdc507032ad4d40f577c41ddb0
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-04-16 21:31:28 +0200

    creation des functions morePostComments et moreChronicComment. séparation des paramètres des requettes sql dans des function private à part

--- apprdp/controllers/Comments.php
+++ apprdp/controllers/Comments.php
@@ -17,6 +17,8 @@ class Comments extends CI_Controller
  */
 	public function postComments()
 	{
+		$_SESSION['comment']['offset'] = 0;
+
 		//paramètres de la requête sql permettant de sélectioner les commentaires liés à un artice donné.
 		$queryParams = array(
 			'select' => 'commentId, commentContent, parentCommentId, commentDate, userFirstName, userLastName, userAvatar',
@@ -25,12 +27,17 @@ class Comments extends CI_Controller
 			'on1' => 'posts_comments.userId = users.userId',
 			'inner1' => 'inner',
 
-			'where' => array('postId' => $_POST['postId']),
+			'where' => array('postId' => $_GET['postId']),
 
 			'order' => 'commentDate DESC'
+
 		);
 
-		$comments = $this->posts_model->getComments($queryParams, 'posts_comments')->result();
+		$_SESSION['comment']['count'] = count($this->posts_model->getComments($queryParams, 'posts_comments')->result());
+
+
+		$comments = $this->posts_model->getComments($queryParams, 'posts_comments',2, 0)->result();
+
 
 		$commentById = [];
 
@@ -46,18 +53,95 @@ class Comments extends CI_Controller
 		}
 
 		//var_dump($comments);
-		$output = '';
+		$output = '<div id="commentsBlock" class="commentsBlock">';
 		foreach ($comments as $row) {
-			$output .= '<div class="eachComment"><div class="auteur">' . $row->userFirstName . ' ' . $row->userLastName . ' le ' . $row->commentDate . '</div> <div class="commentContent">' . $row->commentContent . '</div></div><div class="btnReply"><button type="button" class="reply btn btn-lg btn-sm btn-primary" data-commentId=' . $row->commentId . '>Répondre</button></div>';
+			$output .= '<img class="avatar-mini-comment"  src="/myProjet/webroot/images/usersAvatar/' . $row->userAvatar . '.png" alt="avatar">
+						<div class="eachComment">
+							<div>
+								<div class="auteur">' . $row->userFirstName . ' ' . $row->userLastName . ' le ' . $row->commentDate . '</div>
+								<div class="commentContent">' . $row->commentContent . '</div>
+							</div>
+						</div>
+			<div class="btnReply">
+				<button type="button" class="reply btn btn-lg btn-primary" data-commentId=' . $row->commentId . '>Répondre</button>
+			</div>';
 			if (isset($row->children)) {
 				foreach ($row->children as $row) {
-					$output .= '<div class="eachReply"><div class="auteur">' . $row->userFirstName . ' ' . $row->userLastName . ' le ' . $row->commentDate . '</div> <div class="commentContent">' . $row->commentContent . '</div></div>';
+					$output .= '<div class="eachReply">
+						<div>
+							<div class="auteur">' . $row->userFirstName . ' ' . $row->userLastName . ' le ' . $row->commentDate . '</div>
+							<div class="commentContent">' . $row->commentContent . '</div></div>
+						</div>
+						<img class="avatar-mini-comment"  src="/myProjet/webroot/images/usersAvatar/' . $row->userAvatar . '.png" alt="avatar">';
 				}
 			}
 		}
+		$output .= '</div>';
 		echo $output;
 	}
 
+	public function moreComments()
+    {
+
+
+
+    	//paramètres de la requête sql permettant de sélectioner les commentaires liés à un artice donné.
+    	$queryParams = array(
+    		'select' => 'commentId, commentContent, parentCommentId, commentDate, userFirstName, userLastName, userAvatar',
+
+    		'join1' => 'users',
+    		'on1' => 'posts_comments.userId = users.userId',
+    		'inner1' => 'inner',
+
+    		'where' => array('postId' => $_SESSION['post']['post_id']),
+
+    		'order' => 'commentDate DESC',
+
+    	);
+
+    	$comments = $this->posts_model->getComments($queryParams, 'posts_comments', 2, $_SESSION['comment']['offset']+=2)->result();
+
+		$commentById = [];
+
+		foreach ($comments as  $comment) {
+			$commentById[$comment->commentId] = $comment;
+		}
+
+		foreach ($comments as $key=>$comment) {
+			if ($comment->parentCommentId != 0){
+				$commentById[$comment->parentCommentId]->children[] = $comment;
+				unset($comments[$key]);
+			}
+		}
+
+		//var_dump($comments);
+		$output = '<div id="commentsBlock" class="commentsBlock">';
+		foreach ($comments as $row) {
+			$output .= '<img class="avatar-mini-comment"  src="/myProjet/webroot/images/usersAvatar/' . $row->userAvatar . '.png" alt="avatar">
+						<div class="eachComment">
+							<div>
+								<div class="auteur">' . $row->userFirstName . ' ' . $row->userLastName . ' le ' . $row->commentDate . '</div>
+								<div class="commentContent">' . $row->commentContent . '</div>
+							</div>
+						</div>
+			<div class="btnReply">
+				<button type="button" class="reply btn btn-lg btn-primary" data-commentId=' . $row->commentId . '>Répondre</button>
+			</div>';
+			if (isset($row->children)) {
+				foreach ($row->children as $row) {
+					$output .= '<div class="eachReply">
+						<div>
+							<div class="auteur">' . $row->userFirstName . ' ' . $row->userLastName . ' le ' . $row->commentDate . '</div>
+							<div class="commentContent">' . $row->commentContent . '</div></div>
+						</div>
+						<img class="avatar-mini-comment"  src="/myProjet/webroot/images/usersAvatar/' . $row->userAvatar . '.png" alt="avatar">';
+				}
+			}
+		}
+		$output .= '</div>';
+		echo $output;
+
+    }
 
 
 	/**
@@ -118,7 +202,7 @@ class Comments extends CI_Controller
 				'userId' => $_POST['userId']
 			);
 
-			$this->posts_model->saveNew('posts_comments', $dataToSave);
+			$this->posts_model->saveNew($dataToSave, 'posts_comments');
 		}
 
 	}
@@ -137,7 +221,7 @@ class Comments extends CI_Controller
 				'userId' => $_POST['userId']
 			);
 
-			$this->posts_model->saveNew('chronics_comments', $dataToSave);
+			$this->posts_model->saveNew($dataToSave, 'chronics_comments');
 		}
 
 	}
