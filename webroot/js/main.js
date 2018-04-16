897d7b2327a411bf26ad4fb4b05376b52e20e39c
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-04-16 21:45:23 +0200

    suppression des timers et ajout de nouveaux events

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -12,10 +12,14 @@ $(document).ready(function () {
 	$('#addChronicComment').on('click', addChronicComment);
 
 	//permet d'afficher les commentaires d'un article et d'actualiser les données chaque seconde
-	setInterval('getPostComments()', 1500);
+	// setInterval('getPostComments()', 1500);
+	getPostComments();
 
+	$('#displayPostComments').on('click', '#morePostComments', showMorePostComments);
 	//permet d'afficher les commentaires d'une chronique et d'actualiser les données chaque seconde
-	setInterval('getChronicComments()', 1500);
+	// setInterval('getChronicComments()', 1500);
+	getChronicComments();
+	$('#displayChronicComments').on('click', '#moreChronicComments', showMoreChronicComments);
 
 	//met le focus sur le champ commentaire lorsqu'on clique sur repondre
 	$('main').on('click', '.reply', replyComment);
