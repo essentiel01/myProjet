5a3b7a89e346a1d384bf7a027f53d85d2463d7bc
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-05-10 00:38:18 +0200

    ajout de la pagination

--- apprdp/views/templates/postsArchiveView.php
+++ apprdp/views/templates/postsArchiveView.php
@@ -1,19 +1,23 @@
 <main class="main">
 	<div class="content">
 		<div class="archive">
-			<h2><?= $mainTitle; ?></h2>
-			<table>
-				<?php foreach ($allPosts as $row): ?>
-					<tr>
-						<td>
-							<a href="<?= base_url('culture/publication/' . $row->postId . '/' . $row->postSlug); ?>"><?= $row->countryName ?>: <?= $row->postTitle ?> <?= $row->categoryName ?>  Par <?= $row->writerFirstName . '    ' . $row->writerLastName ?></a>
-						</td>
-						<!-- <td></td>
-						<td></td>
-						<td></td> -->
-					</tr>
-				<?php endforeach; ?>
-			</table>
+			<h2><?php if(isset($mainTitle){echo $mainTitle;}) ?></h2>
+			<?php if ($allPosts != null) { ?>
+				<table>
+					<?php foreach ($allPosts as $row): ?>
+						<tr>
+							<td>
+								<a href="<?= base_url('culture/publication/' . $row->postId . '/' . $row->postSlug); ?>"><?= $row->countryName ?>: <?= $row->postTitle ?> <?= $row->categoryName ?>  Par <?= $row->writerFirstName . '    ' . $row->writerLastName ?></a>
+							</td>
+						</tr>
+					<?php endforeach; ?>
+				</table>
+			<?php } else {
+				echo "<p> $noPosts </p>";
+			 } ?>
 		</div>
+		<!-- pagination -->
+		<?= $this->pagination->create_links(); ?>
+		<!-- /pagination -->
 	</div>
 </main>
