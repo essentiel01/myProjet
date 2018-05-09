0290650838f9af4ff74805aed6aa847e7c1f5be5
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-05-10 00:32:56 +0200

    ajout de la pagination

--- apprdp/views/templates/chronicsArchiveView.php
+++ apprdp/views/templates/chronicsArchiveView.php
@@ -2,18 +2,27 @@
 	<div class="content">
 		<div class="archive">
 			<h2><?= $mainTitle; ?></h2>
-			<table>
-				<?php foreach ($allChronics as $row): ?>
-					<tr>
-						<td>
-							<a href="<?= base_url('culture/chronique/' . $row->chronicId . '/' . $row->chronicSlug); ?>"><?= $row->countryName ?> <?= $row->chronicTitle ?> <?= $row->categoryName ?> Par <?= $row->writerFirstName . '    ' . $row->writerLastName ?></a>
-						</td>
-						<!-- <td></td>
-						<td></td>
-						<td></td> -->
-					</tr>
-				<?php endforeach; ?>
-			</table>
+			<?php if ($allChronics != null) { ?>
+				<table>
+					<?php foreach ($allChronics as $row): ?>
+						<tr>
+							<td>
+								<a href="<?= base_url('culture/chronique/' . $row->chronicId . '/' . $row->chronicSlug); ?>"><?= $row->countryName ?> <?= $row->chronicTitle ?> <?= $row->categoryName ?> Par <?= $row->writerFirstName . '    ' . $row->writerLastName ?></a>
+							</td>
+							<!-- <td></td>
+							<td></td>
+							<td></td> -->
+						</tr>
+					<?php endforeach; ?>
+				</table>
+			<?php }
+			else
+			{
+				echo "<p>  $noChronics </p>";
+			} ?>
 		</div>
+		<!-- pagination -->
+		<?= $this->pagination->create_links(); ?>
+		<!-- /pagination -->
 	</div>
 </main>
