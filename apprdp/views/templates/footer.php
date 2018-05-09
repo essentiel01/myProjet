0b05a07a66a90708e6198b9bfd6938aa94ac4631
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-05-10 00:33:48 +0200

    ajout du contenu du footer

--- apprdp/views/templates/footer.php
+++ apprdp/views/templates/footer.php
@@ -1,8 +1,36 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
 		<!-- footer -->
 		<footer>
-			footer
+			<div class="a-propos">
+				<h3>A propos</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+			<div class="">
+				<ul>
+					<li>
+						<a href="<?= base_url('comment-ca-marche') ?>">Comment ça marche ?</a>
+					</li>
+					<li>
+						<a href="<?= base_url('notre-equipe') ?>">Une équipe de professionnels</a>
+					</li>
+					<li>
+						<a href="<?= base_url('nos-partenaires') ?>">Des partenaires fidèles</a>
+					</li>
+					<li>
+						<a href="<?= base_url('nous-contacter') ?>">Nous contacter</a>
+					</li>
+				</ul>
+			</div>
+			<div class="copyright">
+				<p>Copyright hello Média 2018</p>
+				<span>Site réalisé par: <a href="http://www.cletus-tchekpo.com/" target="blank">Clétus TCHEKPO</span>
+			</div>
 		</footer>
 	</body>
-	<script  src="/myProjet/webroot/js/functions.js"></script>
-	<script  src="/myProjet/webroot/js/main.js"></script>
+	<!-- <script  src="/myProjet/webroot/js/functions.js"></script>
+	<script  src="/myProjet/webroot/js/main.js"></script> -->
 </html>
