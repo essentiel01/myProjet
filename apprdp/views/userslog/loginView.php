2c186cfb088b93034d6bb54a1a3f80405e3ec12d
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-06-10 13:20:06 +0200

    ajout du breadcrumb et modif du formulaire

--- apprdp/views/userslog/loginView.php
+++ apprdp/views/userslog/loginView.php
@@ -1,6 +1,13 @@
 <div class="content">
-	<main>
-		<h2 class="formTitle">Connectez-vous à votre espace personnel</h2>
+	<!--breadcrumb --> 
+	<nav class="breadcrumb-nav" aria-label="breadcrumb">
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item"><a href="<?= base_url('/') ?>">Accueil</a></li>
+			<li class="breadcrumb-item active" aria-current="page">Se connecter</li>
+		</ol>
+	</nav>
+	<main class="main">
+		<h1>Connectez-vous à votre espace personnel</h1>
 		<!-- message d'erreur en cas d'identifiant ou de mot de passe incorrecte -->
 		<p class="formError"><?php if (isset($_SESSION['loginError'])):
 				echo $_SESSION['loginError'];
@@ -8,33 +15,30 @@
 		</p>
 		<!-- formulaire de connexion -->
 		<form class="form" action=<?= base_url('connexion') ?> method="post">
-			<div class="">
-				<div class="label">
-					<label for="email" class="">Email</label>
+			<div class="row form-group">
+				<label for="email" class="col-form-label col-sm-4 col-12">Email</label>
+				<div class="col-sm-8 col-12">
+					<input type="email" class="form-control form-control-lg" id="email" name="email" value="<?php if(form_error('email') == NULL){echo set_value('email');} ?>" placeholder="Email">
+					<span class="formError"><?= form_error('email'); ?></span>
 				</div>
-				<div class="input">
-					<input type="email" class="form-control" id="email" name="email" value="<?php if(form_error('email') == NULL){echo set_value('email');} ?>" placeholder="Email">
-				</div>
-				<span class="formError"><?= form_error('email'); ?></span>
-			</div>
-			<div class="">
-				<div class="label">
-					<label for="password" class="">Mot de passe</label>
-				</div>
-				<div class="input">
-					<input type="password" class="form-control" id="password" name="password" placeholder="Mot de passe">
-				</div>
-				<span class="formError"><?= form_error('password'); ?></span>
 			</div>
 
-			<div class="">
-			    <div class="btnRegister">
-			    	<input type="submit" class="btn btn-primary" value ="Se connecter">
+			<div class="row form-group">
+				<label for="password" class="col-form-label col-sm-4 col-12">Mot de passe</label>
+				<div class="col-sm-8 col-12">
+					<input type="password" class="form-control form-control-lg" id="password" name="password" placeholder="Mot de passe">
+					<span class="formError"><?= form_error('password'); ?></span>
+				</div>
+			</div>
+
+			<div class="row">
+			    <div class="col-sm-4 offset-sm-8 col-12">
+			    	<input type="submit" class="btn btn-lg btn-primary form-control" value ="Se connecter">
 			    </div>
 		  	</div>
 			<!-- liens pour mot de passe oublié et création de compte -->
 			<div class="formLink">
-				<a href="#">Mot de passe oublié?</a>
+				<a href="<?= base_url('mot-de-passe-oublie') ?>">Mot de passe oublié?</a>
 				<a href="<?= base_url('inscription/formulaire') ?>">Je crée mon compte</a>
 			</div>
 		</form>
