2a972c33d6584f3806b4d908de7f8d45f54a16fa
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-06-10 13:04:31 +0200

    ajout du breadcrumb

--- apprdp/views/favorites/index.php
+++ apprdp/views/favorites/index.php
@@ -1,5 +1,12 @@
 <div class="content">
-	<main>
+	<!--breadcrumb --> 
+	<nav class="breadcrumb-nav" aria-label="breadcrumb">
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item"><a href="<?= base_url('/') ?>">Accueil</a></li>
+			<li class="breadcrumb-item active" aria-current="page">Mes favoris</li>
+		</ol>
+	</nav>
+	<main class="main">
 		<div class="indexFavoris">
 			<a href="<?= base_url('espace-personnel/mes-revues-de-presse-favoris') ?>" class="btn btn-lg" role="button">Ma sélection de revues de presse</a>
 			<a href="<?= base_url('espace-personnel/mes-chroniques-favoris') ?>" class="btn btn-lg" role="button">Ma sélection de chronique</a>
