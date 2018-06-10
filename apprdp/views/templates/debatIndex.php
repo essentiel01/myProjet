c90f7a169c8992ec572fb870d21614b49303484b
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-06-10 13:11:11 +0200

    ajout du breadcrumb et réorganisation de la vue

--- apprdp/views/templates/debatIndex.php
+++ apprdp/views/templates/debatIndex.php
@@ -1,33 +1,42 @@
-<main>
-	<h1>Hello débats</h1>
-	<?php 	if ($debats != null) {
-		foreach ($debats as $row) : ?>
-			<div class="">
-				<h2><?= $row->title ?></h2>
-				<p>Publié le <?= $row->posted_at ?></p>
-				<div class="">
-					<div class="person">
-						<img src="/myProjet/webroot/images/web_medias/<?=$row->avatar_1 ?>" alt="avatar">
-						<p><?= $row->person_1 ?></p>
-					</div>
-					<div class="description">
-						<p><?= $row->description ?></p>
-						<a href="<?= base_url('debat/'. $row->slug)?>">Voir</a>
-					</div>
-					<div class="person">
-						<img src="/myProjet/webroot/images/web_medias/<?=$row->avatar_2 ?>" alt="avatar">
-						<p><?= $row->person_2 ?></p>
+<div class="content">
+	<!--breadcrumb --> 
+	<nav class="breadcrumb-nav" aria-label="breadcrumb">
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item"><a href="<?= base_url('/') ?>">Accueil</a></li>
+			<li class="breadcrumb-item active" aria-current="page">Débat</li>
+		</ol>
+	</nav>
+	<main class="main">
+		<h1>Hello débats</h1>
+		<?php 	if ($debats != null) {
+			foreach ($debats as $row) : ?>
+				<div class="debat">
+					<h2><?= $row->title ?></h2>
+					<p>Publié le <?= $row->posted_at ?></p>
+					<div class="">
+						<div class="person">
+							<?=$row->avatar_1 ?>
+							<p><?= $row->person_1 ?></p>
+						</div>
+						<div class="description">
+							<p><?= $row->description ?></p>
+							<a class="btn btn-lg btn-primary" href="<?= base_url('debat/'. $row->slug)?>">Voir</a>
+						</div>
+						<div class="person">
+							<?=$row->avatar_2 ?>
+							<p><?= $row->person_2 ?></p>
+						</div>
 					</div>
 				</div>
-			<?php endforeach;
-		}
-		else
-		{
-			echo '<p>' . $noDebats . '</p>';
-		}?>
+				<?php endforeach;
+			}
+			else
+			{
+				echo '<p>' . $noDebats . '</p>';
+			}?>
 
-		<!-- pagination -->
-		<?= $this->pagination->create_links(); ?>
-		<!-- /pagination -->
-	</div>
-</main>
+			<!-- pagination -->
+			<?= $this->pagination->create_links(); ?>
+			<!-- /pagination -->
+	</main>
+</div>
