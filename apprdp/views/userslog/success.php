e5af9db60563e54548d508cace7df41e74fd8618
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-06-10 13:20:37 +0200

    reorganisation de la vue

--- apprdp/views/userslog/success.php
+++ apprdp/views/userslog/success.php
@@ -1,12 +1,14 @@
 <div class="content">
-	<div class="message">
-		<!-- login de l'utilisateur -->
-		<h2>Bonjour <?= $_SESSION['userData']->userFirstName. ' ' .$_SESSION['userData']->userLastName ?></h2>
-		<div class="">
-			<!-- message d'acceuil -->
-			<p>Content de vous revoir. Pour accéder à votre espace personnel cliquez sur le lien ci-dessous</p>
-			<!-- lien d'accès à l'espace personnel -->
-			<a href="<?= base_url('espace-personnel') ?>">Accéder à mon espace</a>
+	<main class="main">
+		<div class="message">
+			<!-- login de l'utilisateur -->
+			<h2>Bonjour <?= $_SESSION['userData']->userFirstName. ' ' .$_SESSION['userData']->userLastName ?></h2>
+			<div class="">
+				<!-- message d'acceuil -->
+				<p>Content de vous revoir. Pour accéder à votre espace personnel cliquez sur le lien ci-dessous</p>
+				<!-- lien d'accès à l'espace personnel -->
+				<a href="<?= base_url('espace-personnel/profil') ?>">Accéder à mon espace</a>
+			</div>
 		</div>
-	</div>
+	</main>
 </div>
