2995c594250b396a3cf8fcd6b855a407c243a1a3
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-06-10 13:15:54 +0200

    ajout du breadcrumb et réorganisation de la vue

--- apprdp/views/templates/partnersView.php
+++ apprdp/views/templates/partnersView.php
@@ -1,16 +1,26 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<main>
-	<div class="partners">
-		<h1>Partenaires Hello Média</h1>
-		<?php foreach ($partners as $row): ?>
-			<div class="partner">
-				<a href="<?= $row->web ?>">
-					<img src="/myProjet/webroot/images/web_medias/<?= $row->logo ?>" alt="logo">
-				</a>
-				<p><?= $row->name ?></p>
-			</div>
-		<?php endforeach; ?>
-	</div>
-</main>
+<div class="content">
+	<!--breadcrumb --> 
+	<nav class="breadcrumb-nav" aria-label="breadcrumb">
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item"><a href="<?= base_url('/') ?>">Accueil</a></li>
+			<li class="breadcrumb-item active" aria-current="page">Nos partenaires</li>
+		</ol>
+	</nav>
+	<main class="main">
+		<div class="partners">
+			<h1>Partenaires Hello Média</h1>
+			<?php foreach ($partners as $row): ?>
+				<div class="partner">
+					<a href="<?= $row->web ?>">
+						<img src="/myProjet/webroot/images/web_medias/<?= $row->logo ?>" alt="logo">
+					</a>
+					<p><?= $row->name ?></p>
+				</div>
+			<?php endforeach; ?>
+		</div>
+	</main>
+
+</div>
