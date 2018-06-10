04f921b5eae5997d25f1adf40b09b8c66be3b8eb
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-06-10 13:08:07 +0200

    ajout du breadcrumb et reorganisation de la vue

--- apprdp/views/templates/actualiteView.php
+++ apprdp/views/templates/actualiteView.php
@@ -1,14 +1,21 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<main>
-	<div class="">
-			<h2><?= $decodage_actu->title ?></h2>
+<div class="content">
+	<!--breadcrumb --> 
+	<nav class="breadcrumb-nav" aria-label="breadcrumb">
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item"><a href="<?= base_url('/') ?>">Accueil</a></li>
+			<li class="breadcrumb-item active" aria-current="page"><?= $decodage_actu->title ?></li>
+		</ol>
+	</nav>
+	<main class="main">
+			<h1><?= $decodage_actu->title ?></h1>
 			<?php if ( $decodage_actu->updatedAt != null ) { ?>
-				<p><?= $decodage_actu->author ?> modifié le <?= $decodage_actu->updatedAt ?></p>
+				<p class="auteur">Par <?= $decodage_actu->userFirstName . ' ' .  $decodage_actu->userFirstName ?> modifié le <?= $decodage_actu->updatedAt ?></p>
 			<?php } else { ?>
-				<p><?= $decodage_actu->author ?> publié le <?= $decodage_actu->postedAt ?></p>
+				<p class="auteur">Par <?= $decodage_actu->userFirstName . ' ' .  $decodage_actu->userFirstName ?> publié le <?= $decodage_actu->postedAt ?></p>
 			<?php } ?>
 			<p><?= $decodage_actu->content ?></p>
-	</div>
-</main>
+	</main>
+</div>
