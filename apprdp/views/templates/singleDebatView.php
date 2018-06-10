3dc21827d95e55228f4ef8fb9dd0e15f8a4bc134
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-06-10 13:16:37 +0200

    ajout du breadcrumb et réorganisation de la vue

--- apprdp/views/templates/singleDebatView.php
+++ apprdp/views/templates/singleDebatView.php
@@ -1,29 +1,44 @@
-<main>
-	<div class="debat-infos">
-		<h1><?= $debat_infos->title ?></h1>
-		<p>Publié le <?= $debat_infos->posted_at ?></p>
-		<p><?= $debat_infos->description ?></p>
-	</div>
-	<?php foreach ($question_answer as $row) : ?>
-		<h2><?= $row->question ?></h2>
-		<div class="person_1">
-			<div class="">
-				<img src="/myProjet/webroot/images/web_medias/<?= $row->avatar_1 ?>" alt="avatar">
-				<p><?= $row->person_1 ?></p>
-			</div>
-			<div class="answer_1">
-				<p><?= $row->answer_1 ?></p>
-			</div>
+<div class="content">
+	<!--breadcrumb --> 
+	<nav class="breadcrumb-nav" aria-label="breadcrumb">
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item"><a href="<?= base_url('/') ?>">Accueil</a></li>
+			<li class="breadcrumb-item"><a href="<?= base_url('debat') ?>">Débat</a></li>
+			<li class="breadcrumb-item active" aria-current="page">
+			<?= $debat_infos->title ?>
+			</li>
+		</ol>
+	</nav>
+	<main class="main">
+		<div class="debat-infos">
+			<h1 class="debat-title"><?= $debat_infos->title ?></h1>
+			<p>Publié le <?= $debat_infos->posted_at ?></p>
+			<p class="debat-description"><?= $debat_infos->description ?></p>
 		</div>
-		<div class="person_2">
-			<div class="answer_1">
-				<p><?= $row->answer_2 ?></p>
+		<?php foreach ($question_answer as $row) : ?>
+			<div class="questions-answers">
+				<h2 class="question"><?= $row->question ?></h2>
+				<div class="person-1">
+					<div class="person-1-infos">
+						<?= $row->avatar_1 ?>
+						<p><?= $row->person_1 ?></p>
+					</div>
+					<div class="answer-1">
+						<p><?= $row->answer_1 ?></p>
+					</div>
+				</div>
+				<div class="person-2">
+					<div class="answer-2">
+						<p><?= $row->answer_2 ?></p>
+					</div>
+					<div class="person-2-infos">
+						<?= $row->avatar_2 ?>
+						<p><?= $row->person_2 ?></p>
+					</div>
+				</div>
 			</div>
-			<div class="">
-				<img src="/myProjet/webroot/images/web_medias/<?= $row->avatar_2 ?>" alt="avatar">
-				<p><?= $row->person_2 ?></p>
-			</div>
-		</div>
-	<?php endforeach ?>
-	
-</main>
+		<?php endforeach ?>
+
+	</main>
+
+</div>
