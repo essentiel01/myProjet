355e60f76d2cca8798ebf4e1d361725ae311a750
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-06-10 13:12:23 +0200

    ajout du copyright et des scripts externes javascript

--- apprdp/views/templates/footer.php
+++ apprdp/views/templates/footer.php
@@ -4,12 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<!-- footer -->
 		<footer>
 			<div class="a-propos">
-				<h3>A propos</h3>
+				<h3>À propos</h3>
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 				</p>
 			</div>
-			<div class="">
+			<div class="nav-footer">
+				<h3>En savoir plus</h3>
 				<ul>
 					<li>
 						<a href="<?= base_url('comment-ca-marche') ?>">Comment ça marche ?</a>
@@ -26,11 +27,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				</ul>
 			</div>
 			<div class="copyright">
-				<p>Copyright hello Média 2018</p>
-				<span>Site réalisé par: <a href="http://www.cletus-tchekpo.com/" target="blank">Clétus TCHEKPO</span>
+				<p><i class="far fa-copyright"></i> hello Média 2018 - Tous droits réservés</p>
+				<p>Site réalisé par: <a href="http://www.cletus-tchekpo.com/" target="blank">Clétus TCHEKPO</p>
 			</div>
 		</footer>
 	</body>
-	<!-- <script  src="/myProjet/webroot/js/functions.js"></script>
-	<script  src="/myProjet/webroot/js/main.js"></script> -->
+	<script  src="/myProjet/webroot/js/functions.js"></script>
+	<script  src="/myProjet/webroot/js/main.js"></script>
 </html>
