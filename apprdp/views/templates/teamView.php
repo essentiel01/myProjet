997bc53a7d10cd375fe928045209f728d09a0b3b
essentiel01 <tchekpo_cletus@yahoo.fr>
2018-06-10 13:17:42 +0200

    ajout du breadcrumb et réorganisation de la vue

--- apprdp/views/templates/teamView.php
+++ apprdp/views/templates/teamView.php
@@ -1,15 +1,24 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<main>
-	<div class="team">
-		<h1>Equipe Hello Média</h1>
-		<?php foreach ($teams as $row): ?>
-			<div class="member">
-				<img src="/myProjet/webroot/images/usersAvatar/<?= $row->writerAvatar ?>" alt="avatar">
-				<p><?= $row->writerFirstName . ' ' . $row->writerLastName ?></p>
-				<p><?= $row->role ?></p>
-			</div>
-		<?php endforeach; ?>
-	</div>
-</main>
+<div class="content">
+	<!--breadcrumb --> 
+	<nav class="breadcrumb-nav" aria-label="breadcrumb">
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item"><a href="<?= base_url('/') ?>">Accueil</a></li>
+			<li class="breadcrumb-item active" aria-current="page">Notre équipe</li>
+		</ol>
+	</nav>
+	<main class="main">
+		<div class="team">
+			<h1>Equipe Hello Média</h1>
+			<?php foreach ($teams as $row): ?>
+				<div class="member">
+					<img src="/myProjet/webroot/images/usersAvatar/<?= $row->userAvatar ?>" alt="avatar">
+					<p><?= $row->userFirstName . ' ' . $row->userLastName ?></p>
+				</div>
+			<?php endforeach; ?>
+		</div>
+	</main>
+
+</div>
